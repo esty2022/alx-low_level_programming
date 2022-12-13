26bfc7f13cb092037d9b82bf2b580b336febf76e
esty2022 <esther.duntoye@gmail.com>
2022-12-13 10:25:52 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print alphabet - Entry point
+*
+*Return: Always 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
