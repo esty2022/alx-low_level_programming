c6255a7010d2cc05d89a0b952edb823ec4ef9a23
esty2022 <esther.duntoye@gmail.com>
2022-12-13 15:01:45 -0800

    naural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - prints the sum
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((1 % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
