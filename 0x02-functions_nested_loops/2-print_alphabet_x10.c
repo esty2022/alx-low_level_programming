7269509696a857d08af7462a937d07fbb658a9e1
esty2022 <esther.duntoye@gmail.com>
2022-12-13 11:44:55 -0800

    function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print
+*/
+void print_alphabet_x10(void)
+{
+	int t;
+	char l;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
