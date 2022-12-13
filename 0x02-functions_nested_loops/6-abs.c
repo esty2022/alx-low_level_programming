76b490de0e74d7bec27c63b29ce6347e30aac0f9
esty2022 <esther.duntoye@gmail.com>
2022-12-13 13:36:37 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_abs -computes the absolute value
+*@d: the integer
+*Return: The absolute value*
+*/
+int _abs(int d)
+
+	if (d >= 0)
+		return (t);
+	else
+		return (-t);
+}
