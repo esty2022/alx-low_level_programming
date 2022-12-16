10984582eac880496db97dfddde64a7f69b8480d
esty2022 <esther.duntoye@gmail.com>
2022-12-16 08:20:26 -0800

    square

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,19 @@
 */
 void print_diagonal(int n)
 {
-	int i = 0, j;
+	int i = 0, j, a, b;
 
 	while (i < n && n > 0)
 	{
 		j = 0;
 		while (j < i)
 		{
-			_putchar(n + i, n + j);
-			j++;
+			_putchar(a = n + i,b = n + j);
+			a++;
 		}
 		_putchar('\\');
 		_putchar('\n');
-		i++;
+		b++;
 	}
 	if (i == 0)
 		_putchar("\n");
