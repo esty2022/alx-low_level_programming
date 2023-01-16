08af5b18ed96df73670c8c6f6ebf66c69317b901
esty2022 <esther.duntoye@gmail.com>
2023-01-16 06:15:19 -0800

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,27 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - list dates about pet
-* @name: pet name
-* @age: pet age
-* @owner: pet owner name
-*/
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+};
 /**
-* dog - rename strucut with typedef
+* dog_t - typedef for struct dog
 */
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
